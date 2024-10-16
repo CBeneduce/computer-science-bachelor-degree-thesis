7e5840725c95d063b93119941edea4f14d26cb9c
Cristopher Beneduce <c.beneduce@campus.unimib.it>
2024-10-16 10:39:32 +0200

    Fixed category annotation cleaning

--- data-integration/metrics_and_cleaning.py
+++ data-integration/metrics_and_cleaning.py
@@ -4,7 +4,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 integrated_dataset = pd.read_csv('integrated_datasets/category_annotation.csv')
 
 
-# Calculation of precision, recall and f1-score metrics
+# Calculation of precision, recall and F1-score metrics
 original_categories = []
 reconciled_categories = []
 
@@ -23,11 +23,24 @@ print(f'Recall: {recall}')
 print(f'F1-score: {f1}')
 
 
-# Category columns cleaning
-integrated_dataset.drop(columns=['categoria_1', 'categoria_2', 'categoria_3', 'categoria_4',
-                                 'categoria_5', 'categoria_6', 'categoria_7'], inplace=True)
+# Filling of null values in reconciled category columns
+for i in range(1, 8):
+    integrated_dataset[f'categoria_{i}_riconciliata'] = [str(row) for row in integrated_dataset[f'categoria_{i}_riconciliata']]
+
+for row in range(len(integrated_dataset)):
+    for i in range(1, 8):
+        if integrated_dataset.at[row, f'categoria_{i}_riconciliata'] == 'nan':
+            integrated_dataset.at[row, f'categoria_{i}_riconciliata'] = integrated_dataset.at[row, f'categoria_{i}']
+
+
+# Dropping of original category columns
+for i in range(1, 8):
+    integrated_dataset.drop(columns=f'categoria_{i}', inplace=True)
+
 integrated_dataset.columns = integrated_dataset.columns.str.replace('_riconciliata', '')
 
+
+# Dropping of duplicate categories
 categories = []
 
 for row in range(len(integrated_dataset)):
